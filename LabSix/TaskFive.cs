99d064b7727d534eb9e27a25d72b261ace921f79
Michael Melikov <coutaq@gmail.com>
2020-02-05 22:37:31 +0300

    fin.

--- LabSix/TaskFive.cs
+++ LabSix/TaskFive.cs
@@ -7,19 +7,40 @@ namespace LabFive
     internal class TaskFive
     {
         internal static void Execute()
-        { 
-            String bmpPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+"/test.bmp";
-            Console.WriteLine(bmpPath);
+        {
+            String bmpPath;
+            if (!System.Environment.OSVersion.ToString().Contains("Unix"))
+                bmpPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\test.bmp";
+            else
+                bmpPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/test.bmp";
             FileStream bmpFile = new FileStream(bmpPath, FileMode.Open);
-            Bitmap bitmap = new Bitmap(bmpPath, true);
-            Console.WriteLine("Name: "+bmpFile.Name);
-            Console.WriteLine("Size: " + bmpFile.Length);
-            Console.WriteLine("Width: " + bitmap.Width);
-            Console.WriteLine("Height: " + bitmap.Height);
-            Console.WriteLine("Bits per pixel: " + bmpFile.Length/(bitmap.Width*bitmap.Height));
-            Console.WriteLine("Horizontal Resolution: " + bitmap.Height);
-            Console.WriteLine("Vertical Resolution: " + bitmap.Width);
-            //Console.WriteLine("Compression: " + bitmap.);
+            Bitmap bitmap = new Bitmap(bmpFile);
+            Console.WriteLine("Name: " + bmpFile.Name);
+            Console.WriteLine("Size: " + bmpFile.Length + " bytes");
+            Console.WriteLine("Width: " + bitmap.Width + " pixels");
+            Console.WriteLine("Height: " + bitmap.Height + " pixels");
+            byte[] bmpBytes = new byte[bmpFile.Length];
+            bmpFile.Read(bmpBytes, 0, (int)bmpFile.Length);
+            Console.WriteLine("Bits per pixel: " + bmpFile.Length / (bitmap.Width * bitmap.Height));
+            bmpFile.Seek(30, SeekOrigin.Begin);
+            BinaryReader reader = new BinaryReader(bmpFile);
+            Console.Write("Encoding: ");
+            switch (reader.ReadInt32())
+            {
+                case 0:
+                    Console.Write("BI_RGB\n");
+                    break;
+                case 1:
+                    Console.Write("BI_RLE8\n");
+                    break;
+                case 2:
+                    Console.Write("BI_RLE4\n");
+                    break;
+            }
+            reader.ReadInt32();
+            Console.WriteLine("Horizontal Resolution: " + reader.ReadInt32());
+            Console.WriteLine("Vertical Resolution: " + reader.ReadInt32());
+            
         }
     }
 }
